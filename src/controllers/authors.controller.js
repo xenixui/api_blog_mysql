0f0590adac92244cf743dab2148fbed899b278d7
xenixux <kseniakonnova.ux@gmail.com>
2026-05-27 20:56:20 +0200

    authors controllers and models finished

--- src/controllers/authors.controller.js
+++ src/controllers/authors.controller.js
@@ -2,22 +2,53 @@ const AuthorModel = require ('../models/authors.model');
 
 const getAll = async (req, res) => {
     try {
-        const authors = await AuthorModel.getAll();
-        res.json(authors);
+        const result = await AuthorModel.getAll();
+        return res.json(result);
     } catch (error) {
-        res.status(500).json({
+        return res.status(500).json({
             message: 'Error al recuperar los autores',
             error: error.message
         })
     }
 }
 
-const getById = (req, res) => {
-
+const getById = async (req, res) => {
+    try {
+        const result = await AuthorModel.getById(req.params.author_id);
+        if(!result) {
+            return res.status(404).json({
+                message: 'No existe autor con el ID indicado',
+            });
+        }
+        return res.json(result);
+    return res.json(result);
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Error al recuperar el autor',
+            error: error.message
+        });
+    }
+    
 }
 
-const create = (req, res) => {
-
+const create = async (req, res) => {
+    //req.body -> name, email, image 
+    try {
+        const result = await AuthorModel.insert(req.body);
+        const newAuthor = await AuthorModel.getById(result.insertId);
+        
+        if(!newAuthor) {
+            return res.status(404).json({
+                message: 'No existe autor con el ID indicado',
+            });
+        }
+        return res.status(201).json(newAuthor);
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error al crear el autor',
+            error: error.message
+        });
+    }
 }
 
 module.exports = {
--- src/models/authors.model.js
+++ src/models/authors.model.js
@@ -1,10 +1,36 @@
 const db = require('../config/db');
 
 const getAll = async () => {
-    const [result] = await db.query('SELECT name, email, image FROM authors');
+    const [result] = await db.query(`SELECT 
+                                    name, 
+                                    email, 
+                                    image 
+                                    FROM authors`);
+    return result;
+}
+
+const getById = async (author_id) =>{
+    const [result] = await db.query(`SELECT
+                                    name, 
+                                    email, 
+                                    image 
+                                    FROM authors
+                                    WHERE id = ?`,
+                                    [author_id]);
+    if(result.length === 0) return null;
+    return result[0];
+    
+
+}
+
+const insert = async ({name, email, image}) => {
+    const [result] = await db.query(`INSERT INTO authors 
+                                    (name, email, image) 
+                                    VALUES(?, ?, ?)`, 
+                    [name, email, image]);
     return result;
 }
 
 module.exports = {
-    getAll
+    getAll, getById, insert
 }
