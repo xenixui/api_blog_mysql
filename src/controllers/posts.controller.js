83ac00e555b2d2fec26b6c2396f801f8362f4bd3
xenixux <kseniakonnova.ux@gmail.com>
2026-05-27 21:22:02 +0200

    posts controllers and models in progress

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -1,5 +1,15 @@
-const getAll = async (req, res) => {
+const PostModel = require('../models/posts.model');
 
+const getAll = async (req, res) => {
+    try {
+            const result = await PostModel.getAll();
+            return res.json(result);
+        } catch (error) {
+            return res.status(500).json({
+                message: 'Error al recuperar los posts',
+                error: error.message
+            })
+        }
 }
 
 const getByAuthor = (req, res) => {
--- src/models/posts.model.js
+++ src/models/posts.model.js
@@ -1,2 +1,21 @@
 const db = require('../config/db');
 
+const getAll = async () => {
+    const [result] = await db.query(`SELECT 
+                                    posts.id, 
+                                    posts.title, 
+                                    posts.description, 
+                                    categories.name AS category,
+                                    authors.name, 
+                                    authors.email, 
+                                    authors.image,
+                                    posts.created_at
+                                    FROM posts 
+                                    INNER JOIN authors ON posts.authors_id = authors.id
+                                    INNER JOIN categories on posts.categories_id = categories.id;`);
+    return result;
+}
+
+module.exports = {
+    getAll
+}
